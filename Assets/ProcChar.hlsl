293d4457ee014a9bc1f5ef4f63c679d91208eef9
Keijiro Takahashi <keijiro@gmail.com>
2019-12-22 18:05:48 +0900

    initial commit

---
+++ Assets/ProcChar.hlsl
@@ -0,0 +1,11 @@
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+
+void ProcChar_float(float2 uv, float seed, out float output)
+{
+    uint2 pos = uv * 8;
+    pos.x = min(pos.x, 7 - pos.x) ;
+
+    uint iseed = pos.y * 8 + pos.x + seed * 100;
+
+    output = Hash(iseed) < 0.2 + pos.x * 0.2;
+}
